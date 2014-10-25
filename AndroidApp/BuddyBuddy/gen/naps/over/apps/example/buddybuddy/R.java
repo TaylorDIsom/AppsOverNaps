cb8c7dbed31fa068274e2cc06c8d924e83012389
Salim Dabdoub <sdabdoub6@gatech.edu>
2014-10-25 17:42:15 -0400

    Signed-off-by: Salim Dabdoub <sdabdoub6@gatech.edu>

--- AndroidApp/BuddyBuddy/gen/naps/over/apps/example/buddybuddy/R.java
+++ AndroidApp/BuddyBuddy/gen/naps/over/apps/example/buddybuddy/R.java
@@ -28,29 +28,32 @@ public final class R {
         public static final int roundedbutton=0x7f020003;
     }
     public static final class id {
-        public static final int ButtonTest=0x7f080006;
-        public static final int action_settings=0x7f080009;
-        public static final int button=0x7f080008;
+        public static final int ButtonTest=0x7f080007;
+        public static final int action_settings=0x7f08000a;
+        public static final int button=0x7f080009;
         public static final int button1=0x7f080003;
-        public static final int button2=0x7f080007;
-        public static final int container=0x7f080005;
+        public static final int button2=0x7f080008;
+        public static final int container=0x7f080006;
         public static final int editText1=0x7f080001;
         public static final int listView1=0x7f080004;
+        public static final int login_button=0x7f080005;
         public static final int textView1=0x7f080000;
         public static final int textView2=0x7f080002;
     }
     public static final class layout {
         public static final int activity_add_favorite=0x7f030000;
         public static final int activity_favorites=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int fragment_main=0x7f030003;
-        public static final int schedule=0x7f030004;
+        public static final int activity_login=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int fragment_main=0x7f030004;
+        public static final int schedule=0x7f030005;
     }
     public static final class menu {
         public static final int add_favorite=0x7f070000;
         public static final int favorites=0x7f070001;
-        public static final int main=0x7f070002;
-        public static final int schedule=0x7f070003;
+        public static final int login=0x7f070002;
+        public static final int main=0x7f070003;
+        public static final int schedule=0x7f070004;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
@@ -59,6 +62,7 @@ public final class R {
         public static final int favorite_name=0x7f05000a;
         public static final int favorite_title=0x7f050007;
         public static final int hello_world=0x7f050001;
+        public static final int login_button_text=0x7f05000b;
         public static final int schedule_title=0x7f050005;
         public static final int take_home_button=0x7f050003;
         public static final int title_activity_add_favorite=0x7f050009;
